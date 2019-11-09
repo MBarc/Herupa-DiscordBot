fb7a46ea9bd0cbdf0beadf69c027a4b555cb124c
MBarc <42979055+MBarc@users.noreply.github.com>
2019-11-09 10:54:58 -0500

    Changed token = dict{} to auth = dict{}
    Also, capitalized variables with auth to signify that those variables never change.

--- config.py
+++ config.py
@@ -4,9 +4,9 @@ This file will keep track of static settings for Herupa.
 
 
 # This information can  be found on the developer page.
-token = dict{
-  token = '',
-  client_id = 123, 
-  client_secret = '1a2b3c'
+auth = dict{
+  TOKEN = '',
+  CLIENT_ID = 123, 
+  CLIENT_SECRET = '1a2b3c'
   }
   
