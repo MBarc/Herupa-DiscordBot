3ebf072630da9ffc59e7f0debb372df8e95a22f9
MBarc <42979055+MBarc@users.noreply.github.com>
2019-11-09 11:01:42 -0500

    Created main.py
    Main script that holds commands.

---
+++ main.py
@@ -0,0 +1,12 @@
+'''
+Main script that holds commands.
+'''
+
+import discord
+import config # in the same directory ; imports bot_prefix and token
+import asyncio
+
+client = Bot(command_preefix=config.start['bot_prefix']
+
+@client.command(name='birthday',
+                description = '',)
