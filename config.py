cb47697494adedf589da0b2ca16dcadbb30e89b3
MBarc <42979055+MBarc@users.noreply.github.com>
2019-11-09 09:07:49 -0500

    Created config.py
    This file will keep track of static settings for Herupa.

---
+++ config.py
@@ -0,0 +1,12 @@
+'''
+This file will keep track of static settings for Herupa.
+'''
+
+
+# This information can  be found on the developer page.
+token = dict{
+  token = '',
+  client_id = 123, 
+  client_secret = '1a2b3c'
+  }
+  
